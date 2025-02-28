7b89cdd1ede6c16b848c80d5b8e9b2a500431cfe
Louis Tsang <kawingt2@gmail.com>
2025-02-28 14:44:42 +0800

    add a complete message

--- delete_py/delete_download.py
+++ delete_py/delete_download.py
@@ -2,13 +2,15 @@
 import os, shutil
 
 def delete_folder(path):
-    for filename in os.listdir(path):
+    files = os.listdir(path)
+    for filename in files:
         file_path = os.path.join(path, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
+            print(f'Delete {len(files)} from {path}')
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
