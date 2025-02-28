da34780cda7fc1fb2cc1db22b96790229378c8ac
Louis Tsang <kawingt2@gmail.com>
2025-02-28 14:43:08 +0800

    package a util script to be executable in shell

---
+++ delete_py/delete_download.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os, shutil
+
+def delete_folder(path):
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+folder_path = "test"
+
+delete_folder(folder_path)
