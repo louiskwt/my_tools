c7ce233be5652f0f429594d6f0798450d7b5d174
Louis Tsang <kawingt2@gmail.com>
2025-02-28 14:35:34 +0800

    write a delete util in python

---
+++ delete_py/delete_util.py
@@ -0,0 +1,17 @@
+import os, shutil
+
+def delete_folder(path):
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+folder_path = "test_path"
+
+delete_folder(folder_path)
+
