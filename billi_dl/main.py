efa198456d1c0671b2135518521735185e066a2c
tkw <kawint2@gmail.com>
2026-01-07 03:57:36 -0800

    set up path and ask for url

--- billi_dl/main.py
+++ billi_dl/main.py
@@ -2,8 +2,9 @@ import asyncio
 from bilix.sites.bilibili import DownloaderBilibili
 
 async def main():
+    url = input("Enter a series to download: ")
     async with DownloaderBilibili() as d:
-        await d.get_series("https://www.bilibili.com/video/BV1jK4y1N7ST?p=5")
+        await d.get_series(url, "./videos")
 
 
 if __name__ == '__main__':
