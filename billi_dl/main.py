075596ce88ecb48e00b434351fa631d77ff8087c
tkw <kawint2@gmail.com>
2026-01-15 23:33:44 -0800

    simplify and refactor audio download

--- billi_dl/main.py
+++ billi_dl/main.py
@@ -3,7 +3,7 @@ import os
 from bilix.sites.bilibili import DownloaderBilibili
 
 parser = argparse.ArgumentParser(description="Download videos from Bilibili")
-parser.add_argument("-a", "--audio", help="Get the audio only", type=str)
+parser.add_argument("-a", "--audio", action="store_true", help="Get the audio only")
 parser.add_argument("-v", "--video", help="Get a single video", type=str)
 parser.add_argument("-s", "--series", help="Get a series", type=str)
 
@@ -15,9 +15,7 @@ async def main():
         async with DownloaderBilibili() as d:
             d.progress.start()
             if args.video:
-                await d.get_video(args.video, "videos", quality=0)
-            elif args.audio:
-                await d.get_video(args.audio, "videos", only_audio=True)
+                await d.get_video(args.video, "videos", quality=0, only_audio=args.audio)
             elif args.series:
                 await d.get_series(args.series, "videos", quality=0)
             else:
